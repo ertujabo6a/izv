e1cd70d68c066fd023b66251cceaf702f83e3c3b
AalleexxFfrr <dmitrievaleksandr2001@gmail.com>
2023-10-26 02:42:23 +0200

    code: add first version of integrate function

--- part01.py
+++ part01.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 """
-IZV cast1 projektu
-Autor: 
-
-Detailni zadani projektu je v samostatnem projektu e-learningu.
-Nezapomente na to, ze python soubory maji dane formatovani.
-
-Muzete pouzit libovolnou vestavenou knihovnu a knihovny predstavene na prednasce
+IZV First Part of the Project
+Autor: Aleksandr Dmitriev (240259/xdmitr01)
 """
 
 from bs4 import BeautifulSoup
@@ -16,9 +11,28 @@ from numpy.typing import NDArray
 import matplotlib.pyplot as plt
 from typing import List, Callable, Dict, Any
 
+"""
+The aim of this function is to numerically calculate the integral using the approximate so-called rectangle method. 
+You will receive, as input, a function `f` 
+(compatible with NumPy, taking one NumPy array as input and returning a NumPy array of the same size; no testing is required), 
+the start and end of the interval `a` and `b`, and an optional number of steps `steps`. If not set, `steps` will default to 1000. 
+The function will return the value of the definite integral calculated according to the rectangle method formula.
+"""
+def integrate(f: Callable[[np.ndarray], np.ndarray], a: float, b: float, steps=1000) -> float:
+    # Generate points on a linear distribution from 'a' to 'b'
+    x_values = np.linspace(a, b, steps)
 
-def integrate(f: Callable[[NDArray], NDArray], a: float, b: float, steps=1000) -> float:
-    pass
+    # Calculate the function values at all points
+    y_values = f(x_values)
+
+    # Width of one rectangle (step)
+    dx = (b - a) / steps
+
+    # Calculate the integral using the rectangle method
+    integral_value = np.sum(y_values) * dx
+
+    return integral_value
+
 
 
 def generate_graph(a: List[float], show_figure: bool = False, save_path: str | None = None):
