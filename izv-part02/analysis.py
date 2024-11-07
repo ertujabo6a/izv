be7b698dd77203e129d94360b03904141b1e7057
Aleksandr Dmitriev <dmitrievaleksandr2001@gmail.com>
2024-11-07 16:32:13 +0100

    feat: add template and specification for part 2 of project

---
+++ izv-part02/analysis.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3.12
+# coding=utf-8
+
+from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import zipfile
+
+# muzete pridat libovolnou zakladni knihovnu ci knihovnu predstavenou na prednaskach
+# dalsi knihovny pak na dotaz
+
+# Ukol 1: nacteni dat ze ZIP souboru
+
+
+def load_data(filename : str, ds : str) -> pd.DataFrame:
+    pass
+
+# Ukol 2: zpracovani dat
+
+
+def parse_data(df: pd.DataFrame, verbose: bool = False) -> pd.DataFrame:
+    pass
+
+# Ukol 3: počty nehod v jednotlivých regionech podle stavu vozovky
+def plot_state(df: pd.DataFrame, fig_location: str = None,
+                    show_figure: bool = False):
+    pass
+
+# Ukol4: alkohol a následky v krajích
+def plot_alcohol(df: pd.DataFrame, df_consequences : pd.DataFrame, 
+                 fig_location: str = None, show_figure: bool = False):
+    pass
+
+# Ukol 5: Druh nehody (srážky) v čase
+def plot_type(df: pd.DataFrame, fig_location: str = None,
+              show_figure: bool = False):
+    pass
+
+
+if __name__ == "__main__":
+    # zde je ukazka pouziti, tuto cast muzete modifikovat podle libosti
+    # skript nebude pri testovani pousten primo, ale budou volany konkreni
+    # funkce.
+
+    df = load_data("data_23_24.zip", "nehody")
+    df_consequences = load_data("data_23_24.zip", "nasledky")
+    df2 = parse_data(df, True)
+    
+    plot_state(df2, "01_state.png")
+    plot_alcohol(df2, df_consequences, "02_alcohol.png", True)
+    plot_type(df2, "03_type.png")
+
+# Poznamka:
+# pro to, abyste se vyhnuli castemu nacitani muzete vyuzit napr
+# VS Code a oznaceni jako bunky (radek #%%% )
+# Pak muzete data jednou nacist a dale ladit jednotlive funkce
+# Pripadne si muzete vysledny dataframe ulozit nekam na disk (pro ladici
+# ucely) a nacitat jej naparsovany z disku
